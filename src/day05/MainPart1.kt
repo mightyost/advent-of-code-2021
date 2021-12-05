a5bfdbe19b1cf8d58194a061fb142654a834125b
mightyost <osten@forshed.se>
2021-12-05 08:56:14 +0100

    Day 5

---
+++ src/day05/MainPart1.kt
@@ -0,0 +1,86 @@
+package day05p1
+
+import readInput
+import java.lang.Integer.max
+
+fun main() {
+
+//    val data = readInput("day05/data_test")
+    val data = readInput("day05/data")
+
+    val lines = data.map { Line.from(it) }
+
+    val W = lines.maxOf { max(it.x1, it.x2) }
+    val H = lines.maxOf { max(it.y1, it.y2) }
+
+    val board = Board(W, H)
+
+    val a = lines
+        .filter { it.isHorizontal() || it.isVertical() }
+
+    lines
+        .filter { it.isHorizontal() || it.isVertical() }
+        .forEach { board.draw(it) }
+
+//    println(board)
+    println("Intersections: ${board.nbrOfIntersections}")
+}
+
+class Line(
+    val x1: Int,
+    val y1: Int,
+    val x2: Int,
+    val y2: Int
+) {
+    companion object {
+        // 582,570 -> 582,313
+        fun from(input: String): Line {
+            val parts = input.split(" -> ", ",").map { it.toInt() }
+            return Line(parts[0], parts[1], parts[2], parts[3])
+        }
+    }
+
+    fun isHorizontal() = y1 == y2
+    fun isVertical() = x1 == x2
+}
+
+class Board(width: Int, height: Int) {
+    val board = Array(width+1) { Array(height+1) { 0 } }
+
+    val nbrOfIntersections get() = board.flatten().filter { it > 1 }.count()
+
+    fun draw(line: Line) {
+
+        if (line.isHorizontal()) {
+            for(x in line.x1 toward line.x2) {
+                board[x][line.y1] += 1
+            }
+        }
+
+        if (line.isVertical()) {
+            for(y in line.y1 toward line.y2) {
+                board[line.x1][y] += 1
+            }
+        }
+    }
+
+
+    override fun toString(): String {
+
+        val txt = StringBuilder()
+
+        for (y in 0 until board[0].size) {
+            for (x in 0 until board.size) {
+                txt.append(if (board[x][y] == 0) "." else "${board[x][y]}")
+            }
+            txt.append("\n")
+        }
+
+        return txt.toString()
+    }
+}
+
+private infix fun Int.toward(to: Int): IntProgression {
+    val step = if (this > to) -1 else 1
+    return IntProgression.fromClosedRange(this, to, step)
+}
